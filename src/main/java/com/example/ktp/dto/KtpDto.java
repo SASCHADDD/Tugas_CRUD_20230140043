796f77c9d038f5a06f1c2cc959c33d8f3ffb7703
SASCHADDD <saschadanu23@gmail.com>
2026-03-14 02:33:04 +0700

    membuat ktp dto nya

--- src/main/java/com/example/ktp/dto/KtpDto.java
+++ src/main/java/com/example/ktp/dto/KtpDto.java
@@ -1,15 +1,19 @@
 package com.example.ktp.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class KtpDto {
     private Integer id;
     private String nomorKtp;
     private String namaLengkap;
     private String alamat;
-    private Date tanggalLahir;
+    private LocalDate tanggalLahir;
     private String jenisKelamin;
 }
